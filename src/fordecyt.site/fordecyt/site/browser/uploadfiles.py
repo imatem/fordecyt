97095ecefbc9062340f358bc6e582fc5738c4b68
gil-cano <gil.cano@gmail.com>
2017-05-12 17:21:57 -0500

    upload images

--- src/fordecyt.site/fordecyt/site/browser/uploadfiles.py
+++ src/fordecyt.site/fordecyt/site/browser/uploadfiles.py
@@ -61,7 +61,7 @@ class UploadFoldersForm(form.Form):
                     uploadfile.reindexObject()
                     fileRawData.close()
                 # images
-                if file.lower().endswith(('.jpg', '.jpeg', '.gif', '.png')):
+                elif file.lower().endswith(('.jpg', '.jpeg', '.gif', '.png')):
                     uploadfile = api.content.create(
                         type='Image',
                         id=idn.normalize(file),
@@ -72,29 +72,6 @@ class UploadFoldersForm(form.Form):
                         data=open(filename, 'r').read(),
                         filename=unicode(file, 'utf-8'))
                     uploadfile.reindexObject()
-
+                else:
+                    print (os.path.join(root, file))
         logger.info('done.')
-
-
-# def upload_files(self):
-#     year = '2016'
-#     path = '/acerca-de/estructura-interna/secretaria-academica/informes/investigadores'
-#     uploadfolder = api.content.get(path='/'.join((path, year, 'pdf')))
-#     reports = api.content.find(
-#         context=api.content.get(path='/'.join((path, year))),
-#         portal_type='CVFolder')
-#     instancepath = os.path.abspath(os.curdir)
-#     for report in reports:
-#         pdf = '{0}-anual-{1}.pdf'.format(report.id, year)
-#         FILE = os.path.join(instancepath, 'Extensions/informes', pdf)
-#         try:
-#             fileRawData = open(FILE)
-#         except Exception:
-#             print 'File not found: {}'.format(FILE)
-#             continue
-#         uploadfolder.invokeFactory('File', id=report.id, title=report.id)
-#         uploadfolder[report.id].setFile(fileRawData)
-#         uploadfolder[report.id].setTitle(report.id)
-#         uploadfolder[report.id].reindexObject()
-#         fileRawData.close()
-#     print 'Done'
